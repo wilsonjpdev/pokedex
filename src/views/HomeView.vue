598a202a44e7644480ba4d6bef8c30075d7a1cf9
= <=>
2022-11-04 21:50:27 -0300

    Inicio do projeto

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">Palco</div>
+      <div class="col mb-2">Pokedex</div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
