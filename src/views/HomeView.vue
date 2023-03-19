6e325e0726a596a84ce59ea3a18f84f1623d3661
wilsonjp.dev <wilsonjp.dev@outlook.com>
2023-03-19 10:03:35 -0300

    iniciando o projeto

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">Palco</div>
+      <div class="col mb-2">Pokedex</div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView'
+  
+}
+</script>
