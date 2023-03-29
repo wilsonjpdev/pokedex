84e8d5d6110b254ecf7cfbc4bd883008488764b8
wilsonjp.dev <wilsonjp.dev@outlook.com>
2023-03-29 09:13:13 -0300

    Criacao da ordenacao de pokemons

--- src/views/HabilidadesPokemon.vue
+++ src/views/HabilidadesPokemon.vue
@@ -6,7 +6,7 @@
         <table class="table text-white">
             <tbody>
                 <transition-group name="lista">
-                    <tr v-for="(h, indice) in pokemon.habilidades" :key="h">
+                    <tr v-for="(h, indice) in habilidadesOrdenadas" :key="h">
                         <td>{{ h }}</td>
                         <td class="d-flex justify-content-end">
                             <button type="button" class="btn btn-danger btn-sm" @click="$emit('removerHabilidade', indice)">x</button>
@@ -38,6 +38,12 @@ export default ({
         adicionarHabilidade() {
             this.$emit('adicionarHabilidade', this.habilidade)
             this.habilidade = ''
+        }
+    },
+    computed: {
+        habilidadesOrdenadas() {
+            let habilidades = this.pokemon.habilidades
+            return habilidades.sort()
         }
     }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,80 @@
 <template>
   <div class="container">
     <div class="row mt-2">
-      
       <!-- início lado esquerdo -->
       <div class="col mb-2">
         <div class="card palco">
           <div class="card-header"></div>
 
           <div class="card-body bg-pokebola bg-normal">
-            <div class="pokemon">              
+            <div class="pokemon">
               <transition
                 enter-active-class="animate__animated animate__bounce"
                 leave-active-class="animate__animated animate__bounceOut"
               >
-                <img :src="require(`@/assets/imgs/pokemons/${pokemon.imagem}`)" v-if="exibir">
+                <img
+                  :src="require(`@/assets/imgs/pokemons/${pokemon.imagem}`)"
+                  v-if="exibir"
+                />
               </transition>
               <div class="evolucoes">
                 <transition name="fade" v-for="e in pokemon.evolucoes" :key="e">
-                  <img :src="require(`@/assets/imgs/pokemons/${e.toString().padStart(3, '0')}.png`)" v-if="exibir">
+                  <img
+                    :src="
+                      require(`@/assets/imgs/pokemons/${e
+                        .toString()
+                        .padStart(3, '0')}.png`)
+                    "
+                    v-if="exibir"
+                  />
                 </transition>
               </div>
             </div>
           </div>
 
           <div class="card-footer">
-         
-          <nav class="nav nav-pills nav-fill">
-              <router-link class="nav-item nav-link text-white" :to="{path: '/sobre'}" exact-active-class="active">Sobre</router-link>
-              <router-link class="nav-item nav-link text-white" :to="{path: '/status'}" exact-active-class="active">Status</router-link>
-              <router-link class="nav-item nav-link text-white" :to="{path: '/habilidades'}" exact-active-class="active">Habilidades</router-link>
-          </nav>
+            <nav class="nav nav-pills nav-fill">
+              <router-link
+                class="nav-item nav-link text-white"
+                :to="{ path: '/sobre' }"
+                exact-active-class="active"
+                >Sobre</router-link
+              >
+              <router-link
+                class="nav-item nav-link text-white"
+                :to="{ path: '/status' }"
+                exact-active-class="active"
+                >Status</router-link
+              >
+              <router-link
+                class="nav-item nav-link text-white"
+                :to="{ path: '/habilidades' }"
+                exact-active-class="active"
+                >Habilidades</router-link
+              >
+            </nav>
 
-          <div class="detalhes">
-            <router-view v-slot="{ Component }" :pokemon="pokemon" @adicionarHabilidade="adicionarHabilidade" @removerHabilidade="removerHabilidade">
-              <transition  enter-active-class="animate__animated animate__zoomIn">
-                <component :is="Component" />
-              </transition>
-            </router-view>
-          </div>
-
+            <div class="detalhes">
+              <router-view
+                v-slot="{ Component }"
+                :pokemon="pokemon"
+                @adicionarHabilidade="adicionarHabilidade"
+                @removerHabilidade="removerHabilidade"
+              >
+                <transition
+                  enter-active-class="animate__animated animate__zoomIn"
+                >
+                  <component :is="Component" />
+                </transition>
+              </router-view>
+            </div>
           </div>
         </div>
       </div>
       <!-- fim lado esquerdo -->
 
       <!-- início lado direito -->
-      <div class="col mb-2 pokedex">        
+      <div class="col mb-2 pokedex">
         <div class="row">
           <div class="col">
             <h1>Pokédex</h1>
@@ -54,89 +83,146 @@
 
         <div class="row">
           <div class="col">
-            <select class="form-select">
-              <option>Id crescente</option>
-              <option>Id decrescrente</option>
-              <option>De A - Z</option>
+            <select class="form-select" v-model="ordenacao">
+              <option value="">Ordenar Pokémon</option>
+              <option value="1">Id crescente</option>
+              <option value="2">Id decrescrente</option>
+              <option value="3">De A - Z</option>
+              <option value="4">De Z - A</option>
             </select>
           </div>
-        
+
           <div class="col">
-            <input type="text" class="form-control" placeholder="Pesquisar pokémon">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Pesquisar pokémon"
+            />
           </div>
         </div>
 
         <div class="row">
           <div class="pokedex-catalogo">
-
             <!-- início listagem dinâmica -->
-            <div v-for="p in pokemons" :key="p.id" :class="`cartao-pokemon bg-${ p.tipo }`" @click="analisarPokemon(p)">
+            <div
+              v-for="p in pokemons"
+              :key="p.id"
+              :class="`cartao-pokemon bg-${p.tipo}`"
+              @click="analisarPokemon(p)"
+            >
               <h1>{{ p.id }} {{ p.nome }}</h1>
               <span>{{ p.tipo }}</span>
               <div class="cartao-pokemon-img">
-                <transition appear enter-active-class="animate__animated animate__fadeInDown">
-                  <img :src="require(`@/assets/imgs/pokemons/${ p.imagem }`)">
+                <transition
+                  appear
+                  enter-active-class="animate__animated animate__fadeInDown"
+                >
+                  <img :src="require(`@/assets/imgs/pokemons/${p.imagem}`)" />
                 </transition>
               </div>
             </div>
             <!-- fim listagem dinâmica -->
-
           </div>
         </div>
       </div>
       <!-- fim lado direito -->
-
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   data: () => ({
     exibir: false,
     pokemon: {},
-    pokemons: []
+    pokemons: [],
+    ordenacao: "",
   }),
+  watch: {
+    ordenacao(valorNovo) {
+      if (valorNovo == 1) {
+        this.pokemons.sort((proximo, atual) => {
+          if (atual.id < proximo.id) {
+            return 1;
+          } else if (atual.id > proximo.id) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+      if (valorNovo == 2) {
+        this.pokemons.sort((proximo, atual) => {
+          if (atual.id < proximo.id) {
+            return -1;
+          } else if (atual.id > proximo.id) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      if (valorNovo == 3) {
+        this.pokemons.sort((proximo, atual) => {
+          if (atual.nome < proximo.nome) {
+            return 1;
+          } else if (atual.nome > proximo.nome) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+      if(valorNovo == 4) {
+        this.pokemons.sort((proximo, atual) => {
+          if (atual.nome < proximo.nome) {
+            return -1;
+          } else if (atual.nome > proximo.nome) {
+            return 1;
+          }
+          return 0;
+        })
+      }
+    },
+  },
   created() {
-    fetch('http://localhost:3000/pokemons').then( res => {
-      return res.json()
-    }).then(data => {
-      this.pokemons = data
-    })
+    fetch("http://localhost:3000/pokemons")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.pokemons = data;
+      });
   },
   methods: {
     analisarPokemon(p) {
-
       let mudaPokemonAnalisado = false;
       //se o pokemon atual é diferente do clicado
-      if(this.pokemon.id != p.id && this.exibir) {
+      if (this.pokemon.id != p.id && this.exibir) {
         setTimeout(() => {
-          this.analisarPokemon(p)
-        }, 500)
+          this.analisarPokemon(p);
+        }, 500);
 
-        mudaPokemonAnalisado = true
+        mudaPokemonAnalisado = true;
       }
 
       this.pokemon = p;
       this.exibir = !this.exibir;
 
-      if(!this.exibir && !mudaPokemonAnalisado) {
-        this.pokemon = {}
+      if (!this.exibir && !mudaPokemonAnalisado) {
+        this.pokemon = {};
       }
     },
     adicionarHabilidade(habilidade) {
-      if(this.pokemon.habilidades) {
-        this.pokemon.habilidades.push(habilidade)
+      if (this.pokemon.habilidades) {
+        this.pokemon.habilidades.push(habilidade);
       }
     },
     removerHabilidade(indice) {
-      if(this.pokemon.habilidades[indice]) {
-        this.pokemon.habilidades.splice(indice, 1)
+      if (this.pokemon.habilidades[indice]) {
+        this.pokemon.habilidades.splice(indice, 1);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
@@ -146,8 +232,7 @@ body {
 </style>
 
 <style scoped>
-
-@import '~@/assets/css/animacoes.css';
+@import "~@/assets/css/animacoes.css";
 
 .pokedex {
   padding: 20px;
@@ -179,15 +264,15 @@ body {
   box-shadow: 2px 2px 2px rgba(200, 200, 200, 0.77);
 }
 
-.cartao-pokemon h1{
-  color:#fff;
+.cartao-pokemon h1 {
+  color: #fff;
   font-size: 14px;
   margin: 5px 0px 0px 5px;
   padding: 0px;
 }
 
-.cartao-pokemon span{
-  color:#fff;
+.cartao-pokemon span {
+  color: #fff;
   position: absolute;
   background: rgba(255, 255, 255, 0.3);
   font-size: 12px;
@@ -197,9 +282,9 @@ body {
 }
 
 .cartao-pokemon img {
-    max-width:60%;
-    max-height:60%;
-    float: right;
+  max-width: 60%;
+  max-height: 60%;
+  float: right;
 }
 
 .bg-grama {
@@ -207,19 +292,19 @@ body {
 }
 
 .bg-fogo {
-  background-color: #e47373
+  background-color: #e47373;
 }
 
 .bg-agua {
-  background-color: #5a9ed2
+  background-color: #5a9ed2;
 }
 
 .bg-inseto {
-  background-color: #26d3ab
+  background-color: #26d3ab;
 }
 
 .bg-normal {
-  background-color: #cecece
+  background-color: #cecece;
 }
 
 .bg-pokebola {
@@ -259,5 +344,4 @@ body {
   max-width: 100%;
   max-height: 100%;
 }
-
 </style>
